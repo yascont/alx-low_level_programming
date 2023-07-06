b2b5b7446a2cb48aeccd8c5af76839e1bd3205b7
yassine <control.yassine@gmail.com>
2023-07-06 17:40:28 +0100

    pushing all

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,7 +2,8 @@
 
 /**
  * get_bit - returns the value of a bit at a given
- * index.
+ * @n : long int 
+ * @index : uint
  * Return: value of the bit.
  */
 int get_bit(unsigned long int n, unsigned int index)
